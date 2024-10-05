f74f5db972412238e8a035bbc97259551ed0e0fe
daniel-yang1739 <ytyang6690@gmail.com>
2024-10-05 14:07:28 +0800

    add linter and indent-blankline

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,17 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    cmd = "Telescope",
-    keys = {
-        { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "find files" }
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        cmd = "Telescope",
+        keys = {
+            { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "find files" }
+        },
+        opts = function()
+            return {
+                defaults = {
+                    file_ignore_patterns = { "node_modules", ".git" },
+                }
+            }
+        end
     },
 }
