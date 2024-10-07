422264143097ee03ff677995531c1b9fe7739d0b
daniel-yang1739 <ytyang6690@gmail.com>
2024-10-07 14:27:30 +0800

    use ctrlp and settings

--- lua/plugins/find_files.lua
+++ lua/plugins/find_files.lua
@@ -1,4 +1,21 @@
+
 return {
+    {
+        "kien/ctrlp.vim",
+        config = function()
+            -- Set wildignore patterns
+            vim.o.wildignore = vim.o.wildignore .. '/tmp/,.so,.swp,.zip,.,env,*.pyc'
+
+            if vim.fn.executable('ag') then
+                -- Use Ag in CtrlP for listing files. Lightning fast and respects .gitignore
+                vim.g.ctrlp_user_command = 'ag %s -U --skip-vcs-ignores --ignore-dir=node_modules --ignore-dir=__pycache__ -l -g ""'
+
+                -- Set Ag as the grepprg
+                vim.o.grepprg = 'ag --nogroup --nocolor'
+            end
+        end
+    },
+--[[  disable telescope
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
@@ -22,4 +39,5 @@ return {
             }
         end
     },
+]]--
 }
