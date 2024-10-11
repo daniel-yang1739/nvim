841390c1e8ff8ffe2ba276548a2d6fe99f4ac5f4
daniel-yang1739 <ytyang6690@gmail.com>
2024-10-12 02:11:03 +0800

    modify nerd toggle key

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -4,7 +4,7 @@ return {
         "preservim/nerdtree",
         lazy = false,
         keys = {
-            { "<C-n>", "<cmd>NERDTreeToggle<cr>", desc = "nerdtree toggle" },
+            { "<leader>n", "<cmd>NERDTreeToggle<cr>", desc = "nerdtree toggle" },
         },
         config = function()
             -- Read the argument stdin
