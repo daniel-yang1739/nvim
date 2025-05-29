2acab1d5c7ea59b025c490b3301b903f6b7cbdb3
daniel-yang1739 <ytyang6690@gmail.com>
2025-05-29 08:57:43 +0800

    setup ignore files for telescope

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -1,28 +1,5 @@
 return {
   {
-    -- File Explorer --
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    init = function()
-      local keymap = vim.api.nvim_set_keymap
-      local opts = { noremap = true, silent = true }
-      keymap('n', '<leader>n', '<cmd>NvimTreeToggle<cr>', opts)
-    end,
-    config = function()
-      require("nvim-tree").setup({
-        actions = {
-          open_file = {
-            quit_on_open = true,
-          },
-        },
-      })
-    end,
-  },
-  {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -45,46 +22,47 @@ return {
           preview = {
             hide_on_startup = true,
           },
+          file_ignore_patterns = {
+            "node_modules",
+            ".git/",
+            "__pycache__",
+            "local_data",
+            "cache",
+            "lib/materializecss/",
+            "lib/*-dist/",
+          },
         },
         pickers = {
-          --[[
           find_files = {
-            find_command = {
-              "ag",
-              "--silent",
-              "--nocolor",
-              "--follow",
-              "-g", "",
-              "--literal",
-              "--ignore",
-              ".git",
-              "--ignore",
-              "node_modules",
-              "--ignore",
-              "__pycache__",
-            },
+            no_ignore = true,  -- no ignore files in .gitignore
           },
-          ]]--
         },
       }
     end
   },
   {
     --[[
-    -- Find Files --
-    "ctrlpvim/ctrlp.vim",
+    -- File Explorer --
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    init = function()
+      local keymap = vim.api.nvim_set_keymap
+      local opts = { noremap = true, silent = true }
+      keymap('n', '<leader>n', '<cmd>NvimTreeToggle<cr>', opts)
+    end,
     config = function()
-      -- Set wildignore patterns
-      vim.o.wildignore = vim.o.wildignore .. '/tmp/,.so,.swp,.zip,.,env,*.pyc'
-
-      if vim.fn.executable('ag') then
-        -- Use Ag in CtrlP for listing files. Lightning fast and respects .gitignore
-        vim.g.ctrlp_user_command = 'ag %s --skip-vcs-ignores --ignore-dir=node_modules --ignore-dir=__pycache__ -l -g ""'
-
-        -- Set Ag as the grepprg
-        vim.o.grepprg = 'ag --nogroup --nocolor'
-      end
-    end
+      require("nvim-tree").setup({
+        actions = {
+          open_file = {
+            quit_on_open = true,
+          },
+        },
+      })
+    end,
     ]]--
   },
 }
