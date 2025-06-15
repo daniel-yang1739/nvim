52fecd3610cedce8d2c8e460f07d32bfa08dfb24
daniel-yang1739 <ytyang6690@gmail.com>
2025-06-15 14:35:47 +0800

    use mason tools installer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,18 +3,69 @@ return {
   {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
-    config = true,
     opts = {
-      ensure_installed = {
-        "prettier",       -- JS/TS Formatter
-        "isort",          -- Python Import Sorter
-        "yapf",           -- Python Formatter
-        "shellcheck",     -- Shell Linter
-        "hadolint",       -- Haskell Linter
-        "luacheck",       -- Lua Linter
-      },
-      automatic_installation = true,
+      ui = { border = "rounded" },
     },
+    config = function(_, opts)
+      require("mason").setup(opts)
+    end,
+  },
+
+  -- Mason Tool Installer
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    build = ":MasonToolsUpdate",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "prettier",    -- JS/TS Formatter
+          "isort",       -- Python Import Sorter
+          "yapf",        -- Python Formatter
+          "shellcheck",  -- Shell Linter
+          "hadolint",    -- Docker/Haskell Linter
+          "luacheck",    -- Lua Linter
+        },
+        -- if set to true this will check each tool for updates. If updates
+        -- are available the tool will be updated. This setting does not
+        -- affect :MasonToolsUpdate or :MasonToolsInstall.
+        -- Default: false
+        auto_update = false,
+
+        -- automatically install / update on startup. If set to false nothing
+        -- will happen on startup. You can use :MasonToolsInstall or
+        -- :MasonToolsUpdate to install tools and check for updates.
+        -- Default: true
+        run_on_start = true,
+
+        -- set a delay (in ms) before the installation starts. This is only
+        -- effective if run_on_start is set to true.
+        -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+        -- Default: 0
+        start_delay = 3000, -- 3 second delay
+
+        -- Only attempt to install if 'debounce_hours' number of hours has
+        -- elapsed since the last time Neovim was started. This stores a
+        -- timestamp in a file named stdpath('data')/mason-tool-installer-debounce.
+        -- This is only relevant when you are using 'run_on_start'. It has no
+        -- effect when running manually via ':MasonToolsInstall' etc....
+        -- Default: nil
+        debounce_hours = 5, -- at least 5 hours between attempts to install/update
+
+        -- By default all integrations are enabled. If you turn on an integration
+        -- and you have the required module(s) installed this means you can use
+        -- alternative names, supplied by the modules, for the thing that you want
+        -- to install. If you turn off the integration (by setting it to false) you
+        -- cannot use these alternative names. It also suppresses loading of those
+        -- module(s) (assuming any are installed) which is sometimes wanted when
+        -- doing lazy loading.
+        integrations = {
+          ['mason-lspconfig'] = false,  -- Due to conflict with tsserver naming
+          ['mason-null-ls'] = true,
+          ['mason-nvim-dap'] = true,
+        },
+      })
+    end,
   },
 
   -- LSP Installer
