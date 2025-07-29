c97779eed01680369ec3973e7005f8b746a962b0
daniel-yang1739 <ytyang6690@gmail.com>
2025-07-29 11:00:37 +0800

    fix lualine warning

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
         options = {
           icons_enabled = true,
           -- theme = 'everforest',
-          theme = 'tokyonight',
+          theme = 'kanagawa',
           component_separators = { left = '', right = ''},
           section_separators = { left = '', right = ''},
           disabled_filetypes = {
