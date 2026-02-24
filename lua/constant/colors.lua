2ee3fa7f6b0c3d856059d462edf5b4dbd06d1e6c
daniel-yang1739 <ytyang6690@gmail.com>
2026-02-25 01:07:37 +0800

    update get_fg function

--- lua/constant/colors.lua
+++ lua/constant/colors.lua
@@ -1,17 +1,17 @@
 local M = {}
 
-local function get_fg(name, fallback)
+local function get_fg(name)
     local hl = vim.api.nvim_get_hl(0, { name = name, link = true })
     if hl and hl.fg then
         return string.format('#%06x', hl.fg)
     end
-    return fallback
+    return '#FF00FF'
 end
 
 M.git = {
-  add    = get_fg('DiagnosticOk',    '#A3BE8C'),
-  change = get_fg('DiagnosticInfo',  '#7AA2F7'),
-  delete = get_fg('DiagnosticError', '#BF616A'),
+  add    = get_fg('DiagnosticOk'),
+  change = get_fg('DiagnosticInfo'),
+  delete = get_fg('DiagnosticError'),
 }
 
 return M
